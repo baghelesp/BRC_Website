4e52b3fa8812ccf422ec689292be96910587b720
Aditya Bhattad <adityabhattad18@gmail.com>
2023-04-23 12:22:08 +0530

    Sample Change

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${openSans.className}`}
     >
-      BUIDLing 🔨🔨🔨
+      BUIDLing🔨🔨🔨
     </main>
   )
 }
